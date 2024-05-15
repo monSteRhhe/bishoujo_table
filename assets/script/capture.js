72306d076f33b8c88d9f6db9f089ba378231b4d3
kutwss <31578556+monSteRhhe@users.noreply.github.com>
2024-05-16 01:05:05 +0800

    fix
    再次修正导出图像

--- assets/script/capture.js
+++ assets/script/capture.js
@@ -1,14 +1,14 @@
 'use strict';
 
 let filename = 'bishoujo-table.png', // 导出文件名
-    table = document.querySelector('div.root'),
+    table = document.querySelector('div.container'),
     save_multi = 2; // 放大倍数
 
 $('span.export-screenshot').click(function() {
     /* 放大保存 */
     domtoimage.toBlob(table, {
-        width: 1466 * save_multi,
-        height: 774 * save_multi,
+        width: 1340 * save_multi,
+        height: 753 * save_multi,
         style: {
             'transform': `scale(${save_multi})`,
             'transform-origin': 'top left'
