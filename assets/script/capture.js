8dbd204a70e4c6e8bbb9caa9c5c0d3ba2d600b16
kutwss <31578556+monSteRhhe@users.noreply.github.com>
2024-05-16 01:35:15 +0800

    update

--- assets/script/capture.js
+++ assets/script/capture.js
@@ -5,16 +5,18 @@ let filename = 'bishoujo-table.png', // 导出文件名
     save_multi = 2; // 放大倍数
 
 $('span.export-screenshot').click(function() {
-    /* 放大保存 */
-    domtoimage.toBlob(table, {
-        width: 1340 * save_multi,
-        height: 753 * save_multi,
-        style: {
-            'transform': `scale(${save_multi})`,
-            'transform-origin': 'top left'
-        }
-    })
-    .then(function (blob) {
-        window.saveAs(blob, filename);
+    /* 获取原画布尺寸 */
+    domtoimage.toCanvas(table).then(function(canvas) {
+        /* 放大保存 */
+        domtoimage.toBlob(table, {
+            width: canvas.width * save_multi,
+            height: canvas.height * save_multi,
+            style: {
+                'transform': `scale(${save_multi})`,
+                'transform-origin': 'top left'
+            }
+        }).then(function (blob) {
+            window.saveAs(blob, filename);
+        });
     });
 })
